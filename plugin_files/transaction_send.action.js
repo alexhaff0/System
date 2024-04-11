cdb3b9e6b9be8e95b1876ba3b7034b7bd9644c8d
Alex <alex@outdone.co>
2024-04-10 18:38:40 -0700

    bug fix

--- plugin_files/transaction_send.action.js
+++ plugin_files/transaction_send.action.js
@@ -3,28 +3,27 @@ function(instance, properties, context) {
     if (properties.transactions) {
         let transactions = properties.transactions;
 
-        console.group("Transactions");
-        console.log("Input Raw", transactions);
+        // console.group("Transactions");
+        // console.log("Input Raw", transactions);
 
         transactions = JSON.parse(transactions);
-        console.log("Input Parsed", transactions);
+        // console.log("Input Parsed", transactions);
+
+        let transaction_ids = [];
+        transactions.forEach(transaction => {
+            transaction_ids.push(transaction.transaction_id);
+        });
+        // console.log("Transaction IDs", transaction_ids);
+
+        // MARK TRANSACTIONS AS SENT
+        sendTransactions(transaction_ids);  
+
+        // LOG THE NEW QUEUES
+        logTransactionQueues();
+
+        // UPDATE THE OBJECT MAP
+        instance.publishState("transactions_queue", JSON.stringify(window.systemSyncEngine.transactions.queue));
+        instance.publishState("transactions_sent", JSON.stringify(window.systemSyncEngine.transactions.sent));
 
     };
-
 }
-
-
-
-//     const ids = loadBubbleList(properties.ids);
-//     console.log(ids);
-    
-//     sendTransactions(ids);
-    
-//     logTransactionQueues();
-
-//     updateObjectMap(newTransaction);
-
-//     instance.publishState("transactions_queue", JSON.stringify(window.systemSyncEngine.transactions.queue));
-//     instance.publishState("transactions_sent", JSON.stringify(window.systemSyncEngine.transactions.sent));
-
-// }
